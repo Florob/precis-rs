7ddd2af1c39d131dc9ca3b37cd1bfd5a8835c3e5
Florian Zeitz <florob@babelmonkeys.de>
2015-06-18 20:52:34 +0200

    Generate Script table in tblgen

--- tblgen/src/main.rs
+++ tblgen/src/main.rs
@@ -245,6 +245,183 @@ fn is_punctuation(c: u32, gcs: &Vec<GeneralCategory>) -> bool {
     }
 }
 
+fn make_exceptions() -> HashMap<u32, PrecisResult> {
+    let mut exceptions: HashMap<u32, PrecisResult> = HashMap::new();
+    // PVALID -- Would otherwise have been DISALLOWED
+    exceptions.insert(0x00df, PrecisResult::PValid); // LATIN SMALL LETTER SHARP S
+    exceptions.insert(0x03c2, PrecisResult::PValid); // GREEK SMALL LETTER FINAL SIGMA
+    exceptions.insert(0x06fd, PrecisResult::PValid); // ARABIC SIGN SINDHI AMPERSAND
+    exceptions.insert(0x06fe, PrecisResult::PValid); // ARABIC SIGN SINDHI POSTPOSITION MEN
+    exceptions.insert(0x0f0b, PrecisResult::PValid); // TIBETAN MARK INTERSYLLABIC TSHEG
+    exceptions.insert(0x3007, PrecisResult::PValid); // IDEOGRAPHIC NUMBER ZERO
+
+    // CONTEXTO -- Would otherwise have been DISALLOWED
+    exceptions.insert(0x00b7, PrecisResult::ContextO); // MIDDLE DOT
+    exceptions.insert(0x0375, PrecisResult::ContextO); // GREEK LOWER NUMERAL SIGN (KERAIA)
+    exceptions.insert(0x05f3, PrecisResult::ContextO); // HEBREW PUNCTUATION GERESH
+    exceptions.insert(0x05f4, PrecisResult::ContextO); // HEBREW PUNCTUATION GERSHAYIM
+    exceptions.insert(0x30fb, PrecisResult::ContextO); // KATAKANA MIDDLE DOT
+
+    // CONTEXTO -- Would otherwise have been PVALID
+    exceptions.insert(0x0660, PrecisResult::ContextO); // ARABIC-INDIC DIGIT ZERO
+    exceptions.insert(0x0661, PrecisResult::ContextO); // ARABIC-INDIC DIGIT ONE
+    exceptions.insert(0x0662, PrecisResult::ContextO); // ARABIC-INDIC DIGIT TWO
+    exceptions.insert(0x0663, PrecisResult::ContextO); // ARABIC-INDIC DIGIT THREE
+    exceptions.insert(0x0664, PrecisResult::ContextO); // ARABIC-INDIC DIGIT FOUR
+    exceptions.insert(0x0665, PrecisResult::ContextO); // ARABIC-INDIC DIGIT FIVE
+    exceptions.insert(0x0666, PrecisResult::ContextO); // ARABIC-INDIC DIGIT SIX
+    exceptions.insert(0x0667, PrecisResult::ContextO); // ARABIC-INDIC DIGIT SEVEN
+    exceptions.insert(0x0668, PrecisResult::ContextO); // ARABIC-INDIC DIGIT EIGHT
+    exceptions.insert(0x0669, PrecisResult::ContextO); // ARABIC-INDIC DIGIT NINE
+    exceptions.insert(0x06f0, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT ZERO
+    exceptions.insert(0x06f1, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT ONE
+    exceptions.insert(0x06f2, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT TWO
+    exceptions.insert(0x06f3, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT THREE
+    exceptions.insert(0x06f4, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT FOUR
+    exceptions.insert(0x06f5, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT FIVE
+    exceptions.insert(0x06f6, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT SIX
+    exceptions.insert(0x06f7, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT SEVEN
+    exceptions.insert(0x06f8, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT EIGHT
+    exceptions.insert(0x06f9, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT NINE
+
+    // DISALLOWED -- Would otherwise have been PVALID
+    exceptions.insert(0x0640, PrecisResult::Disallowed); // ARABIC TATWEEL
+    exceptions.insert(0x07fa, PrecisResult::Disallowed); // NKO LAJANYALAN
+    exceptions.insert(0x302e, PrecisResult::Disallowed); // HANGUL SINGLE DOT TONE MARK
+    exceptions.insert(0x302f, PrecisResult::Disallowed); // HANGUL DOUBLE DOT TONE MARK
+    exceptions.insert(0x3031, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK
+    exceptions.insert(0x3032, PrecisResult::Disallowed); // VERTICAL KANA REPEAT
+                                                         // WITH VOICED SOUND MARK
+    exceptions.insert(0x3033, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK UPPER HALF
+    exceptions.insert(0x3034, PrecisResult::Disallowed); // VERTICAL KANA REPEAT
+                                                         // WITH VOICED SOUND MARK UPPER HA
+    exceptions.insert(0x3035, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK LOWER HALF
+    exceptions.insert(0x303b, PrecisResult::Disallowed); // VERTICAL IDEOGRAPHIC ITERATION MARK
+
+    exceptions
+}
+
+fn write_precis_table<W: Write>(out: &mut W, gcs: &Vec<GeneralCategory>, jcs: &HashSet<u32>,
+                                noncharacters: &HashSet<u32>,
+                                default_ignorable: &HashSet<u32>,
+                                hangul_syllable_type: &HashMap<u32, HangulSyllableType>,
+                                exceptions: &HashMap<u32, PrecisResult>,
+                                back_compat: &HashMap<u32, PrecisResult>) {
+    let (x, y, z) = unicode_normalization::UNICODE_VERSION;
+    writeln!(out, "// Generated from Unicode {}.{}.{}{}", x, y, z, r#"
+use std::cmp::Ordering::{Less, Equal, Greater};
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum PrecisResult {
+    PValid,
+    FreePValid,
+    ContextJ,
+    ContextO,
+    Unassigned,
+    Disallowed
+}
+
+pub fn precis_result(c: char) -> PrecisResult {
+    let c = c as u32;
+    match PRECIS_RESULTS.binary_search_by(|&(lo, hi, _)| {
+        if hi < c { Less }
+        else if lo <= c && c <= hi { Equal }
+        else { Greater }
+    }) {
+        Ok(idx) => PRECIS_RESULTS[idx].2,
+        Err(_) => unreachable!()
+    }
+}
+"#).unwrap();
+
+    writeln!(out, "pub static PRECIS_RESULTS: &'static [(u32, u32, PrecisResult)] = &[").unwrap();
+    let mut prev = None;
+    let mut i = 0u32;
+    for cp in 0u32..(0x10ffff + 1) {
+        let result = if let Some(&res) = exceptions.get(&cp) {
+            res
+        } else if let Some(&res) = back_compat.get(&cp) {
+            res
+        } else if is_unassigned(cp, &gcs, &noncharacters) {
+            PrecisResult::Unassigned
+        } else if is_ascii7(cp) {
+            PrecisResult::PValid
+        } else if is_join_control(cp, &jcs) {
+            PrecisResult::ContextJ
+        } else if is_old_hangul_jamo(cp, &hangul_syllable_type) {
+            PrecisResult::Disallowed
+        } else if is_precis_ignorable(cp, &default_ignorable, &noncharacters) {
+            PrecisResult::Disallowed
+        } else if is_control(cp, &gcs) {
+            PrecisResult::Disallowed
+        } else if is_compat(cp) {
+            PrecisResult::FreePValid
+        } else if is_letter_digits(cp, &gcs) {
+            PrecisResult::PValid
+        } else if is_other_letter_digits(cp, &gcs) {
+            PrecisResult::FreePValid
+        } else if is_spaces(cp, &gcs) {
+            PrecisResult::FreePValid
+        } else if is_symbols(cp, &gcs) {
+            PrecisResult::FreePValid
+        } else if is_punctuation(cp, &gcs) {
+            PrecisResult::FreePValid
+        } else {
+            PrecisResult::Disallowed
+        };
+
+        if prev != Some(result) {
+            if let Some(ty) = prev {
+                println!("{:04X} {}", cp - 1, ty);
+                write!(out, "0x{:04x}, PrecisResult::{:?}),", cp - 1, ty).unwrap();
+                i += 1;
+                if i % 2 == 1 { writeln!(out, "").unwrap(); }
+            }
+            if i == 0 || i % 2 == 1 { write!(out, "   ").unwrap(); }
+            print!("{:04X}-", cp);
+            write!(out, " (0x{:04x}, ", cp).unwrap();
+        }
+        prev = Some(result);
+    }
+    println!("{:04X} {}", 0x10ffff, prev.unwrap());
+    writeln!(out, "0x{:04x}, PrecisResult::{:?})", 0x10ffff, prev.unwrap()).unwrap();
+    writeln!(out, "];").unwrap();
+}
+
+fn write_script_table<W: Write>(out: &mut W, script: &Vec<(u32, u32, String)>) {
+    let scripts: HashSet<&String> = script.iter().map(|&(_, _, ref x)| x).collect();
+
+    writeln!(out, "{}", r#"
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Script {"#).unwrap();
+    for s in scripts {
+        writeln!(out, "    {},", s).unwrap();
+    }
+    writeln!(out, "    Unknown").unwrap();
+    writeln!(out, "}}").unwrap();
+
+    writeln!(out, "{}", r#"
+pub fn script(c: char) -> Script {
+    let c = c as u32;
+    match SCRIPT.binary_search_by(|&(lo, hi, _)| {
+        if hi < c { Less }
+        else c < lo { Greater }
+        else { Equal }
+    }) {
+        Ok(idx) => SCRIPT[idx].2,
+        Err(_) => Script::Unknown
+    }
+}
+"#).unwrap();
+
+    writeln!(out, "pub static SCRIPT: &'static [(u32, u32, Script)] = &[").unwrap();
+
+    for &(lo, hi, ref s) in script {
+        writeln!(out, "    (0x{:04x}, 0x{:04x}, Script::{}),", lo, hi, s).unwrap();
+    }
+    writeln!(out, "];").unwrap();
+}
+
 fn main() {
     const SEPS: &'static [char] = &[';', '#'];
 
@@ -341,138 +518,32 @@ fn main() {
         }
     });
 
-    let mut exceptions: HashMap<u32, PrecisResult> = HashMap::new();
-    // PVALID -- Would otherwise have been DISALLOWED
-    exceptions.insert(0x00df, PrecisResult::PValid); // LATIN SMALL LETTER SHARP S
-    exceptions.insert(0x03c2, PrecisResult::PValid); // GREEK SMALL LETTER FINAL SIGMA
-    exceptions.insert(0x06fd, PrecisResult::PValid); // ARABIC SIGN SINDHI AMPERSAND
-    exceptions.insert(0x06fe, PrecisResult::PValid); // ARABIC SIGN SINDHI POSTPOSITION MEN
-    exceptions.insert(0x0f0b, PrecisResult::PValid); // TIBETAN MARK INTERSYLLABIC TSHEG
-    exceptions.insert(0x3007, PrecisResult::PValid); // IDEOGRAPHIC NUMBER ZERO
+    let mut script: Vec<(u32, u32, String)> = Vec::new();
+    parse_props_file("Scripts.txt", |first, last, prop| {
+        let prop = prop.replace("_", "");
+        script.push((first, last.unwrap_or(first), prop));
+    });
+    script.sort_by(|x, y| x.0.cmp(&y.0));
+    let script = script.into_iter().fold(Vec::new(), |mut acc: Vec<(u32, u32, String)>, elem| {
+        let mut same = false;
+        if let Some(last) = acc.last_mut() {
+            if last.2 == elem.2 && last.1 == elem.0 - 1 {
+                last.1 = elem.1;
+                same = true;
+            }
+        }
+        if same { return acc; }
+        acc.push(elem);
+        acc
+    });
 
-    // CONTEXTO -- Would otherwise have been DISALLOWED
-    exceptions.insert(0x00b7, PrecisResult::ContextO); // MIDDLE DOT
-    exceptions.insert(0x0375, PrecisResult::ContextO); // GREEK LOWER NUMERAL SIGN (KERAIA)
-    exceptions.insert(0x05f3, PrecisResult::ContextO); // HEBREW PUNCTUATION GERESH
-    exceptions.insert(0x05f4, PrecisResult::ContextO); // HEBREW PUNCTUATION GERSHAYIM
-    exceptions.insert(0x30fb, PrecisResult::ContextO); // KATAKANA MIDDLE DOT
-
-    // CONTEXTO -- Would otherwise have been PVALID
-    exceptions.insert(0x0660, PrecisResult::ContextO); // ARABIC-INDIC DIGIT ZERO
-    exceptions.insert(0x0661, PrecisResult::ContextO); // ARABIC-INDIC DIGIT ONE
-    exceptions.insert(0x0662, PrecisResult::ContextO); // ARABIC-INDIC DIGIT TWO
-    exceptions.insert(0x0663, PrecisResult::ContextO); // ARABIC-INDIC DIGIT THREE
-    exceptions.insert(0x0664, PrecisResult::ContextO); // ARABIC-INDIC DIGIT FOUR
-    exceptions.insert(0x0665, PrecisResult::ContextO); // ARABIC-INDIC DIGIT FIVE
-    exceptions.insert(0x0666, PrecisResult::ContextO); // ARABIC-INDIC DIGIT SIX
-    exceptions.insert(0x0667, PrecisResult::ContextO); // ARABIC-INDIC DIGIT SEVEN
-    exceptions.insert(0x0668, PrecisResult::ContextO); // ARABIC-INDIC DIGIT EIGHT
-    exceptions.insert(0x0669, PrecisResult::ContextO); // ARABIC-INDIC DIGIT NINE
-    exceptions.insert(0x06f0, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT ZERO
-    exceptions.insert(0x06f1, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT ONE
-    exceptions.insert(0x06f2, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT TWO
-    exceptions.insert(0x06f3, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT THREE
-    exceptions.insert(0x06f4, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT FOUR
-    exceptions.insert(0x06f5, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT FIVE
-    exceptions.insert(0x06f6, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT SIX
-    exceptions.insert(0x06f7, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT SEVEN
-    exceptions.insert(0x06f8, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT EIGHT
-    exceptions.insert(0x06f9, PrecisResult::ContextO); // EXTENDED ARABIC-INDIC DIGIT NINE
-
-    // DISALLOWED -- Would otherwise have been PVALID
-    exceptions.insert(0x0640, PrecisResult::Disallowed); // ARABIC TATWEEL
-    exceptions.insert(0x07fa, PrecisResult::Disallowed); // NKO LAJANYALAN
-    exceptions.insert(0x302e, PrecisResult::Disallowed); // HANGUL SINGLE DOT TONE MARK
-    exceptions.insert(0x302f, PrecisResult::Disallowed); // HANGUL DOUBLE DOT TONE MARK
-    exceptions.insert(0x3031, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK
-    exceptions.insert(0x3032, PrecisResult::Disallowed); // VERTICAL KANA REPEAT
-                                                         // WITH VOICED SOUND MARK
-    exceptions.insert(0x3033, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK UPPER HALF
-    exceptions.insert(0x3034, PrecisResult::Disallowed); // VERTICAL KANA REPEAT
-                                                         // WITH VOICED SOUND MARK UPPER HA
-    exceptions.insert(0x3035, PrecisResult::Disallowed); // VERTICAL KANA REPEAT MARK LOWER HALF
-    exceptions.insert(0x303b, PrecisResult::Disallowed); // VERTICAL IDEOGRAPHIC ITERATION MARK
-
+    let exceptions = make_exceptions();
     let back_compat: HashMap<u32, PrecisResult> = HashMap::new();
 
     let mut out = File::create("precis-table.rs").unwrap();
+    write_precis_table(&mut out, &gcs, &jcs, &noncharacters, &default_ignorable,
+                       &hangul_syllable_type, &exceptions, &back_compat);
 
-    writeln!(out, "// Generated from Unicode {}.{}.{}{}", x, y, z, r#"
-use std::cmp::Ordering::{Less, Equal, Greater};
+    write_script_table(&mut out, &script);
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum PrecisResult {
-    PValid,
-    FreePValid,
-    ContextJ,
-    ContextO,
-    Unassigned,
-    Disallowed
-}
-
-pub fn precis_result(c: char) -> PrecisResult {
-    let c = c as u32;
-    match PRECIS_RESULTS.binary_search_by(|&(lo, hi, _)| {
-        if hi < c { Less }
-        else if lo <= c && c <= hi { Equal }
-        else { Greater }
-    }) {
-        Ok(idx) => PRECIS_RESULTS[idx].2,
-        Err(_) => unreachable!()
-    }
-}
-"#).unwrap();
-
-    writeln!(out, "pub static PRECIS_RESULTS: &'static [(u32, u32, PrecisResult)] = &[").unwrap();
-    let mut prev = None;
-    let mut i = 0u32;
-    for cp in 0u32..(0x10ffff + 1) {
-        let result = if let Some(&res) = exceptions.get(&cp) {
-            res
-        } else if let Some(&res) = back_compat.get(&cp) {
-            res
-        } else if is_unassigned(cp, &gcs, &noncharacters) {
-            PrecisResult::Unassigned
-        } else if is_ascii7(cp) {
-            PrecisResult::PValid
-        } else if is_join_control(cp, &jcs) {
-            PrecisResult::ContextJ
-        } else if is_old_hangul_jamo(cp, &hangul_syllable_type) {
-            PrecisResult::Disallowed
-        } else if is_precis_ignorable(cp, &default_ignorable, &noncharacters) {
-            PrecisResult::Disallowed
-        } else if is_control(cp, &gcs) {
-            PrecisResult::Disallowed
-        } else if is_compat(cp) {
-            PrecisResult::FreePValid
-        } else if is_letter_digits(cp, &gcs) {
-            PrecisResult::PValid
-        } else if is_other_letter_digits(cp, &gcs) {
-            PrecisResult::FreePValid
-        } else if is_spaces(cp, &gcs) {
-            PrecisResult::FreePValid
-        } else if is_symbols(cp, &gcs) {
-            PrecisResult::FreePValid
-        } else if is_punctuation(cp, &gcs) {
-            PrecisResult::FreePValid
-        } else {
-            PrecisResult::Disallowed
-        };
-
-        if prev != Some(result) {
-            if let Some(ty) = prev {
-                println!("{:04X} {}", cp - 1, ty);
-                write!(out, "0x{:04x}, PrecisResult::{:?}),", cp - 1, ty).unwrap();
-                i += 1;
-                if i % 2 == 1 { writeln!(out, "").unwrap(); }
-            }
-            if i == 0 || i % 2 == 1 { write!(out, "   ").unwrap(); }
-            print!("{:04X}-", cp);
-            write!(out, " (0x{:04x}, ", cp).unwrap();
-        }
-        prev = Some(result);
-    }
-    println!("{:04X} {}", 0x10ffff, prev.unwrap());
-    writeln!(out, "0x{:04x}, PrecisResult::{:?})", 0x10ffff, prev.unwrap()).unwrap();
-    writeln!(out, "];").unwrap();
 }
